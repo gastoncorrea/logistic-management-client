da0c9c0bc5fbf8921101422484dc2a6026212a8b
Gaston <gastoncorrea90e@gmail.com>
2025-01-30 11:58:12 -0300

    create method get Http

---
+++ src/app/core/services/order-data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from  '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDataService {
+
+  constructor(private http:HttpClient) { }
+  URL = "http://localhost:5000/"
+  ordersData():Observable<any>{
+    return this.http.get(this.URL)
+  }
+}
