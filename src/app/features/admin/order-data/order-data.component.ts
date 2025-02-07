604f58dfd84f8f2be19cd0f81bcf918a1e5cbdc9
Gaston <gastoncorrea90e@gmail.com>
2025-02-06 23:06:13 -0300

    try to order detail view

--- src/app/features/admin/order-data/order-data.component.ts
+++ src/app/features/admin/order-data/order-data.component.ts
@@ -11,6 +11,7 @@ export class OrderDataComponent implements OnInit {
   constructor(private orderDataService:OrderDataService) { }
   orderData:any = [];
   orderDetail:any = [];
+  orderDetailView:any;
   ngOnInit(): void {
     this.getData();
   }
@@ -22,11 +23,20 @@ export class OrderDataComponent implements OnInit {
     })
   }
 
-  selectOrder(id_pedido:number){
-    console.log("probando el evento: ",id_pedido);
-    this.orderDataService.orderDetail(id_pedido).subscribe((res)=>{
+  selectOrder(pedido:any){
+    console.log("probando el evento: ",pedido);
+    this.orderDataService.orderDetail(pedido.id_pedido).subscribe((res)=>{
       this.orderDetail = res;
       console.log(this.orderDetail);
+      this.orderDetailView = {
+        nro_pedido : pedido.nro_pedido,
+        fecha:pedido.fecha,
+        nombre_cliente: pedido.id_cliente,
+        detalle_pedido: this.orderDetail,
+        ubicacion: pedido.id_ubicacion
+      }
+      console.log(this.orderDetailView.detalle_pedido)
     })
+    ;
   }
 }
